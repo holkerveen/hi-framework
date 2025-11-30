a2840149b080ac4c0da0ee009fa573f7102ffc64
Henk van de Berg <11218586+holkerveen@users.noreply.github.com>
2025-11-30 16:24:52 +0100

    Introducing bootstrap, use on dashboard

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -5,14 +5,15 @@ namespace Framework\Controllers;
 
 use Framework\Attributes\Route;
 use Psr\Log\LoggerInterface;
+use Twig\Environment;
 
 class DashboardController
 {
     #[Route('/')]
-    public function index(LoggerInterface $logger): string
+    public function index(LoggerInterface $logger, Environment $twig): string
     {
         $logger->info('Cool!', ["file"=>__FILE__]);
-        return "Welcome to Dashboard!";
+        return $twig->render('dashboard.html.twig');
     }
 
 }
