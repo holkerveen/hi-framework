c6f2f17ca712ec9fb01cc624df8a19936b0b87ca
Henk van de Berg <11218586+holkerveen@users.noreply.github.com>
2025-11-30 15:32:29 +0100

    Reuse comment form, move all html to twig

--- src/Controllers/CommentController.php
+++ src/Controllers/CommentController.php
@@ -36,42 +36,30 @@ class CommentController
     }
     
     #[Route('/comments/{id}/edit')]
-    public function edit(string $id, EntityStorageInterface $store): string {
+    public function edit(string $id, EntityStorageInterface $store, Environment $twig): string {
         $comment = $store->read(Comment::class, $id);
-        return "<form action='/comments/".htmlspecialchars($id, ENT_QUOTES)."/store' method='post'>
-            <label style='display:block;'>Name<input name='name' value='".htmlspecialchars($comment->getName(), ENT_QUOTES)."'/></label>
-            <label style='display:block;'>Email<input name='email' value='".htmlspecialchars($comment->getEmail(), ENT_QUOTES)."'/></label>
-            <label style='display:block;'>Message<textarea name='message'/>".htmlspecialchars($comment->getMessage(),ENT_QUOTES)."</textarea></label>
-            <button>Save</button>
-            </form>";
+        return $twig->render("comments/edit.html.twig", compact("comment"));
     }
     
     #[Route('/comments/{id}/store')]
-    public function store(string $id, EntityStorageInterface $store): string {
+    public function store(string $id, EntityStorageInterface $store, Environment $twig): string {
         $comment = $store->read(Comment::class, $id);
         $comment->setName($_POST['name']);
         $comment->setEmail($_POST['email']);
         $comment->setMessage($_POST['message']);
         $store->update($comment);
-        return "<h1>Comment updated</h1><p><a href='/comments'>to list</a></p>";
+        return $twig->render("comments/store.html.twig", compact("comment"));
     }
     
     #[Route('/comments/{id}/confirm-delete')]
-    public function confirmDelete(string $id, EntityStorageInterface $store): string {
+    public function confirmDelete(string $id, EntityStorageInterface $store, Environment $twig): string {
         $comment = $store->read(Comment::class, $id);
-        return "
-            <p>You are about to delete comment '".htmlspecialchars($comment->getName(), ENT_QUOTES)."', from '".$comment->getEmail()."'. This cannot be undone. Are you sure?</p>
-            <a href=\"/comments\">Cancel</a>
-            <form action='/comments/".htmlspecialchars($comment->getId(), ENT_QUOTES)."/delete' method='post'>
-                <input type='hidden' name='id' value='".htmlspecialchars($id, ENT_QUOTES)."' />
-                <button type='submit'>Delete</button>
-                </form>
-            </form>";
+        return $twig->render("comments/confirm-delete.html.twig", compact("comment"));
     }
 
     #[Route('/comments/{id}/delete')]
-    public function delete(string $id, EntityStorageInterface $store): string {
+    public function delete(string $id, EntityStorageInterface $store, Environment $twig): string {
         $store->delete($store->read(Comment::class, $id));
-        return "<h1>Comment deleted</h1><p><a href='/comments'>to list</a></p>";
+        return $twig->render("comments/delete.html.twig");
     }
 }
