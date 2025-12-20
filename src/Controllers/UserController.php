89ea8fb6b70f04a07ad7db4ba2b44fa8b90336f3
Henk van de Berg <11218586+holkerveen@users.noreply.github.com>
2025-12-20 17:37:36 +0100

    Authentication: Add users CRUD

---
+++ src/Controllers/UserController.php
@@ -0,0 +1,72 @@
+<?php
+// src/Controllers/UserController.php
+
+namespace Framework\Controllers;
+
+use Framework\Attributes\Route;
+use Framework\Entity\User;
+use Framework\Storage\EntityStorageInterface;
+use Twig\Environment;
+
+class UserController
+{
+    #[Route('/users')]
+    public function index(EntityStorageInterface $store, Environment $twig): string
+    {
+        return $twig->render("users/index.html.twig", [
+            'users' => $store->index(User::class),
+        ]);
+    }
+
+    #[Route('/users/create')]
+    public function create(Environment $twig): string
+    {
+        return $twig->render("users/create.html.twig");
+    }
+
+    #[Route('/users/post')]
+    public function post(EntityStorageInterface $store, Environment $twig): string
+    {
+        $user = new User();
+        $user->setEmail($_POST['email']);
+        $user->setPassword($_POST['password']);
+        $store->create($user);
+
+        return $twig->render("users/post.html.twig");
+    }
+
+    #[Route('/users/{id}/confirm-delete')]
+    public function confirmDelete(string $id, EntityStorageInterface $store, Environment $twig): string
+    {
+        $user = $store->read(User::class, $id);
+        return $twig->render("users/confirm-delete.html.twig", compact("user"));
+    }
+    
+    #[Route('/users/{id}/delete')]
+    public function delete(string $id, EntityStorageInterface $store, Environment $twig): string
+    {
+        $user = $store->read(User::class, $id);
+        $store->delete($user);
+        return $twig->render("users/delete.html.twig", compact("user"));
+    }
+    
+    #[Route('/users/{id}/edit')]
+    public function edit(string $id, EntityStorageInterface $store, Environment $twig): string
+    {
+        return $twig->render("users/edit.html.twig", [
+            'user' => $store->read(User::class, $id),
+        ]);
+    }
+    
+    #[Route('/users/{id}/put')]
+    public function put(string $id, EntityStorageInterface $store, Environment $twig): string
+    {
+        $user = $store->read(User::class, $id);
+        $user->setEmail($_POST['email']);
+        if(!empty($_POST['password'])) {
+            $user->setPassword($_POST['password']);
+        }
+        $store->update($user);
+        return $twig->render("users/put.html.twig", compact("user"));
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Framework\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Framework\Storage\EntityInterface;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'users')]
+class User implements EntityInterface
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'string')]
+    private string $id;
+    #[ORM\Column]
+    private string $email;
+    #[ORM\Column]
+    private string $password;
+    
+    public function setId(string $id) {
+        $this->id = $id;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+    
+    public function getId(): string
+    {
+        return $this->id;
+    }
+    
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    
+    public function verifyPassword(string $password): bool {
+        return password_verify($password, $this->password);
+    }
+}
