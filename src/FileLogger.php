dc917ea279d8b6a8f70053a3abac1553f896eeb8
Henk van de Berg <11218586+holkerveen@users.noreply.github.com>
2026-01-10 11:34:51 +0100

    Simplify FileLogger by using PSR abstract class

--- src/FileLogger.php
+++ src/FileLogger.php
@@ -3,11 +3,12 @@
 namespace Hi;
 
 use Hi\Attributes\Service;
+use Psr\Log\AbstractLogger;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
 #[Service]
-class FileLogger implements LoggerInterface
+class FileLogger extends AbstractLogger implements LoggerInterface
 {
     private ?string $logFile;
 
@@ -21,46 +22,6 @@ class FileLogger implements LoggerInterface
                 mkdir($directory, 0755, true);
             }
         }
-    }
-
-    public function emergency($message, array $context = []): void
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    public function alert($message, array $context = []): void
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    public function critical($message, array $context = []): void
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    public function error($message, array $context = []): void
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    public function warning($message, array $context = []): void
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    public function notice($message, array $context = []): void
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    public function info($message, array $context = []): void
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    public function debug($message, array $context = []): void
-    {
-        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
     public function log($level, $message, array $context = []): void
