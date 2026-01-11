346663e1ba0492bbcf0682bbb32add617040dc10
Henk van de Berg <11218586+holkerveen@users.noreply.github.com>
2026-01-12 00:17:19 +0100

    Better error reporting for Config

--- src/Config.php
+++ src/Config.php
@@ -7,7 +7,7 @@ use Exception;
 
 class Config implements ArrayAccess
 {
-    public function __construct(private ?array $config = null)
+    public function __construct(private ?array $config = null, private array $path = [])
     {
         if (is_null($this->config)) {
             $this->config = require(PathHelper::getBasedir() . DIRECTORY_SEPARATOR . 'config.php');
@@ -21,8 +21,12 @@ class Config implements ArrayAccess
 
     public function offsetGet(mixed $offset): Config|string|bool|float|int
     {
+        $path = [...$this->path, $offset];
+        if(!array_key_exists($offset, $this->config)) {
+            throw new Exception("Config option ".implode('.',$path)." does not exist.");
+        }
         if (is_array($this->config[$offset])) {
-            return new Config($this->config[$offset]);
+            return new Config($this->config[$offset], $path);
         }
         return $this->config[$offset];
     }
